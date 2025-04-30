b65ffd8a53faeb68638772ea46e8cc6dc94494ff
Caio Rosa <dev.caiorosa@gmail.com>
2025-04-30 12:41:17 +0100

    Fix formatting issues in config.py and run.py; update requirements.txt

--- api/config.py
+++ api/config.py
@@ -11,4 +11,4 @@ class Config:
         f"mysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}"
         f"@{os.getenv('MYSQL_HOST')}/{os.getenv('MYSQL_DB')}"
     )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False 
+  SQLALCHEMY_TRACK_MODIFICATIONS = False 
